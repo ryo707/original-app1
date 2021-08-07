742b28170ac5328851e1e62f6be5ce09684cc11e
ryo707 <74702059+yu-dari@users.noreply.github.com>
2021-08-07 10:12:45 +0900

    投稿編集機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,16 @@ class PostsController < ApplicationController
     post = Post.find(params[:id])
     post.destroy
   end
-  
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+  end
+
   private
   def post_params
     params.require(:post).permit(:nickname, :image, :title, :text)
