cd872ba32b322b916bfe5eedd30ec8ad7cd93ec0
ryo707 <74702059+yu-dari@users.noreply.github.com>
2021-08-16 18:55:59 +0900

    投稿検索機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   validates :title, :text, presence: true
   belongs_to :user
   has_many :comments
+
+  def self.search(search)
+    if search != ""
+      Post.where('text LIKE(?)', "%#{search}%")
+    else
+      Post.all
+    end
+  end
 end
