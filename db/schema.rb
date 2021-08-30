d553e3e2db4fe8b8dfadba4eef31d65378cd6519
ryo707 <74702059+yu-dari@users.noreply.github.com>
2021-08-30 23:59:57 +0900

    評価機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_22_152526) do
+ActiveRecord::Schema.define(version: 2021_08_30_145930) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_08_22_152526) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.integer "id"
+    t.float "rate"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
